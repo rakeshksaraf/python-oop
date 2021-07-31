c4f9bd14c1be6d19e7718a18f46ae12a59c989d1
rakeshksaraf@gmail.com <rakeshksaraf@gmail.com>
2021-07-30 18:34:41 -0700

    added method breed

--- oop1.py
+++ oop1.py
@@ -2,6 +2,8 @@ class Dog:
     def __init__(self,name):
         self.name=name
         print(name)
+    def breed(self,which):
+	return which
     def bark(self):
         print("woof") 
     def legs(self,x):
@@ -13,3 +15,4 @@ print(type(d1))
 d1.bark()
 print(d1.legs(4))
 print(d1.food("bone"))
+print(d1.breed("husky"))
