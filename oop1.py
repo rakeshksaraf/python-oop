c3220d203dfd8c79e64dbb75199e999700845ab5
rakeshksaraf@gmail.com <rakeshksaraf@gmail.com>
2021-07-30 18:23:26 -0700

    added food method

--- oop1.py
+++ oop1.py
@@ -1,12 +1,15 @@
 class Dog:
-    def __init__(self,who):
-        self.ron=who
-        print(self.ron)
+    def __init__(self,name):
+        self.name=name
+        print(name)
     def bark(self):
         print("woof") 
     def legs(self,x):
         return x
+    def eat(self, food):
+	return food
 d1= Dog("ruby")
 print(type(d1))
 d1.bark()
 print(d1.legs(4))
+print(d1.food("bone"))
