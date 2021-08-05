4dab02c94688af9e48bd0076866ce19d4db9bc19
rakeshksaraf@gmail.com <rakeshksaraf@gmail.com>
2021-08-04 22:02:50 -0700

    added a userdefined halfway method

--- distance.py
+++ distance.py
@@ -17,10 +17,15 @@ class Point:
         return d
     def distance(self,p2):
         return (round(math.sqrt((self.x-p2.x)**2-(self.y-p2.y)**2),2))
+    def halfway(self,target):
+        midX=(self.x+target.x)/2.0
+        midY=(self.y+target.y)/2.0
+        return Point(midX,midY)
 p1=Point(4,3)
-p2=Point(2,2)
+p2=Point(2,4)
 print(p1)
 print(p1.x)
 print(p1.distance_origin())
 print(p1.distance(p2))
 print(p1-p2)
+print(p1.halfway(p2))
